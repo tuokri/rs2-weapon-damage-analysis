642340fb0574a629d43caa45be65804393b29420
Tuomo <tuokri@tuta.io>
2022-08-16 20:58:49 +0300

    Load external scripts

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
+import json
+import os
+from typing import Dict
+from typing import List
+
 import dash
 import plotly.express as px
 
-app = dash.Dash("RS2 Weapons")
+
+def load_external_scripts(var: str) -> List[Dict]:
+    scripts = os.environ.get(var)
+    if not scripts:
+        return []
+    return json.loads(scripts)
+
+
+external_scripts = load_external_scripts("EXTERNAL_SCRIPTS")
+
+app = dash.Dash(
+    "RS2 Weapon Simulator",
+    external_scripts=external_scripts,
+)
 
 fig = px.line()
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(debug=False)
