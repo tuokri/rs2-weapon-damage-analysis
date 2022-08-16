9a04b27cd061b32df06d078c70f15dfb9ee9271c
Tuomo <tuokri@tuta.io>
2022-08-16 21:54:26 +0300

    Some style tweaks

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from typing import Dict
 from typing import List
 
 import dash
+import dash_bootstrap_components as dbc
 import plotly.express as px
 from dash import dcc
 from dash import html
@@ -21,6 +22,7 @@ external_scripts = load_external_scripts("EXTERNAL_SCRIPTS")
 app = dash.Dash(
     __name__,
     external_scripts=external_scripts,
+    external_stylesheets=[dbc.themes.VAPOR],
     title="rs2sim",
 )
 server = app.server
@@ -28,17 +30,14 @@ server = app.server
 fig = px.line(x=[0, 1], y=[1, 2])
 
 app.layout = html.Div(
-    className="container",
     children=[
         html.H1(
             children="Rising Storm 2: Vietnam Weapon Simulator",
         ),
         html.Div(
-            className="container",
             children="Work in progress."
         ),
         dcc.Graph(
-            className="container",
             id="test-graph",
             figure=fig
         ),
