7a8c7b0d1c8b2995b33bc0f21a97364e950ff086
Tuomo <tuokri@tuta.io>
2022-08-19 21:34:20 +0300

    Refactoring and clean up

---
+++ pages/privacypolicy.py
@@ -0,0 +1,14 @@
+import dash
+import dash_bootstrap_components as dbc
+
+from dash import html
+
+dash.register_page(__name__)
+
+layout = dbc.Container([
+    html.H2(children="TODO: PRIVACY POLICY"),
+
+    html.Div(children="""
+        TODO CONTENT.
+    """),
+])
